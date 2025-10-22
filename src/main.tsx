fef51abd9ec3590dca4279e29114761675e83b7e
Aaron  Coreas <a.coreas@ethnixgroup.com>
2025-10-22 13:41:53 -0600

    fix: main.tsx - simplify structure and correct App import

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AuthProvider } from './contexts/AuthContext'
 import App from './App'
 import './index.css'
-
-const queryClient = new QueryClient()
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
